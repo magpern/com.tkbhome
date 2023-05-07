f64b59b84c7773f5a9fcb75d2b17b2eb04eac502
pshotton <phil.shotton@cloudscapesolutions.com>
2023-05-07 14:03:40 +0100

    Added debug capability

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const Homey = require('homey');
 
 class TKBHomeApp extends Homey.App {
 	onInit() {
+		// Start debuger
+		if (process.env.DEBUG === '1') {
+			require('inspector').open(9229, '0.0.0.0', true);
+			//require(“inspector”).open(9229, “0.0.0.0”, true);
+		}
 		this.log(`${Homey.manifest.id} running...`);
 	}
 }
